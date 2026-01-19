afab83a80086d87b4fb5707c777af3556d0cb399
Muhammad Rizqi Winassyabani <124354652+haruza123@users.noreply.github.com>
2026-01-19 15:50:06 +0700

    hari 3 (baru lagi)

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-document.querySelectorAll('a[href^="#"]').forEach(link=>{
-  link.addEventListener('click',e=>{
-    const target=document.querySelector(link.getAttribute('href'));
-    if(!target) return;
+document.querySelectorAll('a[href^="#"]').forEach((link) => {
+  link.addEventListener("click", (e) => {
+    const target = document.querySelector(link.getAttribute("href"));
+    if (!target) return;
     e.preventDefault();
     window.scrollTo({
-      top:target.offsetTop-80,
-      behavior:"smooth"
+      top: target.offsetTop - 80,
+      behavior: "smooth",
     });
   });
+});
+
+const toggle = document.querySelector(".nav-toggle");
+const nav = document.querySelector(".nav");
+
+toggle.addEventListener("click", () => {
+  nav.classList.toggle("open");
 });
